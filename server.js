f5eff392f88351cf5903996132487f1f69af8cf8
ymasog <yer.masog@gmail.com>
2021-02-03 21:59:36 -0600

    created one more api route for homepage

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ var notes = []
 
 //Routes
 // * GET `/notes` - Should return the `notes.html` file.
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
